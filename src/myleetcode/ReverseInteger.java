168b2481ce71275c43ae36a9729c885c76a726b8
programmerhjh <605594106@qq.com>
2018-09-22 11:42:11 +0800

    日常水题

---
+++ src/myleetcode/ReverseInteger.java
@@ -0,0 +1,39 @@
+package myleetcode;
+
+/**
+ * 反转整数
+ * @author acer
+ *
+ */
+public class ReverseInteger {
+
+	private static StringBuffer stringBuffer = new StringBuffer();
+	
+	public static void main(String[] args) {
+		System.out.println(reverse(0));
+	}
+	
+	public static int reverse(int x) {
+    
+		Long result = 0L;
+		
+		if (x >= 0) {
+			String str = stringBuffer.append(String.valueOf(x)).reverse().toString();
+			result = Long.parseLong(str);
+		} else {
+			String valueOf = String.valueOf(x);
+			String substring = valueOf.substring(1, valueOf.length());
+			String str = stringBuffer.append(substring).reverse().insert(0, "-").toString();
+			result = Long.parseLong(str);
+		}
+
+		stringBuffer.delete(0, stringBuffer.length());
+
+		if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+			return 0;
+		}
+		
+		return result.intValue();
+    }
+	
+}
---
+++ src/myleetcode/StringToIntegerAtoi.java
@@ -0,0 +1,45 @@
+package myleetcode;
+
+import java.math.BigInteger;
+
+/**
+ * 字符串转整数 
+ * @author acer
+ *
+ */
+public class StringToIntegerAtoi {
+
+	public static void main(String[] args) {
+
+		System.out.println(myAtoi("      -42"));
+		System.out.println('a' - '1');
+	}
+	
+	
+	public static int myAtoi(String str) {
+		str = str.trim();
+		String[] split = str.split("([a-z][A-Z]{0,})|([A-Z][a-z]{0,})|\\ |\\.");
+		if (split.length != 0 && split[0] != null) {
+			String string = split[0].trim();
+			if (string.length() > 2 && string.lastIndexOf("-") != 0 && string.lastIndexOf("-") != -1) {
+				string = string.substring(0, string.indexOf("-", 1));
+			} else if (string.length() > 2 && string.lastIndexOf("+") != 0 && string.lastIndexOf("+") != -1) {
+				string = string.substring(0, string.indexOf("+", 1));
+			}
+			if (string.matches("(\\-{0,1}[0-9]{1,})|(\\+{0,1}[0-9]{1,})")) {
+				BigInteger bigInteger = new BigInteger(string);
+				if (bigInteger.compareTo(new BigInteger(String.valueOf(Integer.MIN_VALUE))) == -1) {
+					return Integer.MIN_VALUE;
+				} else if (bigInteger.compareTo(new BigInteger(String.valueOf(Integer.MAX_VALUE))) == 1) {
+					return Integer.MAX_VALUE;
+				}
+				if (str.indexOf(string) == 0) {
+					return Integer.parseInt(string);
+				}
+			} else {
+				return 0;
+			}
+		}
+		return 0;
+    }
+}
