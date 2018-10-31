48a902b390b92d621cf201a58279744f8f3fbb20
programmerhjh <605594106@qq.com>
2018-10-31 20:26:13 +0800

    日常水题（腾讯秋招50道）

---
+++ src/myleetcode/LinkedListCycle.java
@@ -0,0 +1,27 @@
+package myleetcode;
+
+/**
+ * 环形链表
+ * 追赶相遇
+ * @author acer
+ *
+ */
+public class LinkedListCycle {
+
+	public boolean hasCycle(ListNode head) {
+		if (head == null || head.next ==null || head.next.next ==null)
+            return false;
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast.next != null && fast.next.next != null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow){
+                return true;
+            }
+        }
+        return false;
+    }
+	
+}
+
