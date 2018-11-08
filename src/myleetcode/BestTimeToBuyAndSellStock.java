f2581b3fff518da31491cad55123cc86df192fa2
programmerhjh <605594106@qq.com>
2018-11-08 20:55:43 +0800

    日常水题（腾讯秋招50道）

---
+++ src/myleetcode/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,22 @@
+package myleetcode;
+
+/**
+ * 买卖股票的最佳时机
+ * @author acer
+ *
+ */
+public class BestTimeToBuyAndSellStock {
+	
+	public int maxProfit(int[] prices) {
+		int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
+    }
+	
+}
---
+++ src/myleetcode/BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,29 @@
+package myleetcode;
+
+/**
+ * 买卖股票的最佳时机 II
+ * @author acer
+ *
+ */
+public class BestTimeToBuyAndSellStockII {
+	
+	public int maxProfit(int[] prices) {
+		if (prices.length == 0) {
+			return 0;
+		}
+		int i = 0;
+        int valley = prices[0];
+        int peak = prices[0];
+        int maxprofit = 0;
+        while (i < prices.length - 1) {
+            while (i < prices.length - 1 && prices[i] >= prices[i + 1])
+                i++;
+            valley = prices[i];
+            while (i < prices.length - 1 && prices[i] <= prices[i + 1])
+                i++;
+            peak = prices[i];
+            maxprofit += peak - valley;
+        }
+        return maxprofit;
+	}
+}
